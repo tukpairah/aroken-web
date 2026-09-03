348946d8134274e6a063a74a7dac8ff8d5dfe983
macintoshi31 <136407516+tukpairah@users.noreply.github.com>
2026-09-03 13:53:43 +0500

    Update main-17.js

--- lesson-17/main-17.js
+++ lesson-17/main-17.js
@@ -3,9 +3,9 @@
 const number = 7;
 
 if (number % 2 === 0) {
-	console.log("Число " + number + "четное");
+	console.log("Число " + number + " четное");
 } else {
-	console.log("Число " + number + "нечетное");
+	console.log("Число " + number + " нечетное");
 }
 
 // Задача 2.
